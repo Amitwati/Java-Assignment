fcf9882fec624212f8e2d0dde0b630d5a20bb116
Amitwati <amitwati69@gmail.com>
2017-06-06 03:12:28 +0300

    linux modification

--- src/graphics/IDrawable.java
+++ src/graphics/IDrawable.java
@@ -1,9 +1,11 @@
 package graphics;
 
 import java.awt.Graphics;
+import java.io.File;
 
 public interface IDrawable {
-	 public final static String PICTURE_PATH = "src\\pictures\\";
+	//when working on linux the saperate should be '/' | on windows '\\'
+	public final static String PICTURE_PATH = "src/pictures/";
 	 public void loadImages(String nm);
 	 public void drawObject(Graphics g);
 	 public String getColor();	 
